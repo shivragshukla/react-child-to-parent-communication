2d9ecfdb2e53b5901b52b1b0c533dd25142c04bc
Shivrag <shivragshukla001@gmail.com>
2024-02-22 13:58:03 +0530

    Updated: child and parent component

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react"
+
 export default function Child({getData}) {
 
-    getData('This is a Child data')
+    const [input, setInput] = useState('');
+    
+    useEffect(() => {
+        getData(input)
+    } ,[input])
 
     return (
         <>
-            This is a Child component
+            <div style={{border: '1px solid #CCC', padding : '1rem', margin: '1rem',  backgroundColor: '#FFF'}}>
+                <h3>This is a Child component</h3>
+                <input 
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    style={{padding: '0.5rem'}}
+                />
+            </div>
+            
         </>
     )
 }
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Child from "./Child";
 
 export default function Parent() {
 
+    const [childText, setChildText] = useState('')
+
     function getData(params) {
-        console.log(params);
+        setChildText(params)
     }
 
     return (
         <>
-            <Child getData={getData} />
-            Hello Parent
+            <div style={{border: '1px solid #CCC', padding : '1rem', margin: '1rem', backgroundColor: '#FAFAFA'}}>
+                <h3>This is a Parent component</h3>
+                <p>{ childText }</p>
+                <Child getData={getData} />
+            </div>
+            
         </>
     )
 }
